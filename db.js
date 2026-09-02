4c4be21eb56b81b82247846bc452aa2e364a7a4f
AbhishekMishra <abhishekmishra@P2E-0224.local>
2026-09-02 17:23:52 +0530

    added changed fix

--- db.js
+++ db.js
@@ -3,12 +3,12 @@ import pg from "pg";
 
 const { Pool } = pg;
 
+const DATABASE_URL =
+  process.env.DATABASE_URL ||
+  "postgresql://postgres:root@localhost:5432/postgres";
+
 export const pool = new Pool({
-  host: process.env.POSTGRES_HOST || "localhost",
-  port: Number(process.env.POSTGRES_PORT) || 5432,
-  user: process.env.POSTGRES_USER || "postgres",
-  password: process.env.POSTGRES_PASSWORD || "root",
-  database: process.env.POSTGRES_DB || "rback-app",
+  connectionString: DATABASE_URL,
 });
 
 export async function checkDb() {
